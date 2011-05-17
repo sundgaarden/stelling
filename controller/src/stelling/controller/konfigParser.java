2365b0582b6e17e8abc39bf629896b990acc27c0
Christian Hansen <sundgaarden@gmail.com>
2011-05-18 00:51:33 +0200

    tilfOjet metode til hente konfig filer

---
+++ controller/src/stelling/controller/konfigParser.java
@@ -0,0 +1,39 @@
+package stelling.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * 
+ * @author Christian
+ * Det antages at mappen (konfig) med konfigurationsfilerne 
+ * ligger i samme mappe som den executable jar fil.
+ * 
+ */
+
+public class konfigParser {
+
+
+/**
+ * Returnerer et array af konfig filer
+ * 
+ */
+
+public File[] getKonfigFiler() throws FileNotFoundException{ 
+	//sti til executable jar fil hentes
+	new File(System.getProperty("java.class.path"));
+	//sti til konfig filer angives
+	File bibliotek = new File("path/konfig");
+	File[] listeAfFiler = bibliotek.listFiles();
+	return listeAfFiler;
+}
+
+/**
+ * Henter konfiguration - to be implemented
+ */
+
+}
+
+	
+	
+	
