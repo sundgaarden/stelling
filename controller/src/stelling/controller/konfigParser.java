b9c22850959637d6c5a3a0c76508b604ace3bbd5
Christian Hansen <sundgaarden@gmail.com>
2011-05-24 17:18:58 +0200

    added konfig.xml samt updateret konfigParser - den er dog ikke helt oppe at Kore endnu

--- controller/src/stelling/controller/konfigParser.java
+++ controller/src/stelling/controller/konfigParser.java
@@ -1,36 +1,129 @@
 package stelling.controller;
 
+import stelling.model.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+
+import javax.lang.model.element.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * 
  * @author Christian
- * Det antages at mappen (konfig) med konfigurationsfilerne 
- * ligger i samme mappe som den executable jar fil.
+ * 
+ * 
  * 
  */
 
 public class konfigParser {
 
 
+private Document dom;
+private Collection<ValgAttributType> myAtt;
+private List<String> feltNavne = new ArrayList<String> ();
+private List<String> attributter = new ArrayList<String> ();
+
 /**
  * Returnerer et array af konfig filer
  * 
  */
 
-private File[] getKonfigFiler() throws FileNotFoundException{ 
-	//sti til executable jar fil hentes
-	new File(System.getProperty("java.class.path"));
+//private File[] getKonfigFiler() throws FileNotFoundException{ 
+	////sti til executable jar fil hentes
+	//new File(System.getProperty("java.class.path"));
 	//sti til konfig filer angives
-	File bibliotek = new File("path/konfig");
-	File[] listeAfFiler = bibliotek.listFiles();
-	return listeAfFiler;
-}
+	//File bibliotek = new File("path/konfig");
+	//File[] listeAfFiler = bibliotek.listFiles();
+	//return listeAfFiler;
+//}
 
 /**
  * Henter konfiguration - to be implemented
  */
+public void parseXmlFil(){
+	//hent document builder factory
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+try{	
+	DocumentBuilder db = dbf.newDocumentBuilder();
+	//parse konfig filen med builderen for at hente dom repræsentation af xml filen
+	dom = db.parse("konfig.xml");
+
+}catch(ParserConfigurationException pce) {
+	pce.printStackTrace();
+}catch(SAXException se) {
+	se.printStackTrace();
+}catch(IOException ioe) {
+	ioe.printStackTrace();
+}
+}
+
+
+private void parseDokumentet(){
+	//hent root elementet
+	org.w3c.dom.Element dokEle = dom.getDocumentElement();
+	
+	//hent nodeliste af elementer
+	NodeList nl = dokEle.getElementsByTagName("attributTypeValg");
+	if(nl !=null && nl.getLength()>0){
+		for(int i = 0 ; i < nl.getLength();i++) {
+			
+			//hent attributTypeValg elementet
+			Element el = (Element)nl.item(i);
+			
+			//hent attributTypeValg objektet
+			ValgAttributType a = getAttributTypeValg(el);
+			
+			//add til liste
+			myAtt.add(a);
+		}
+	}
+}
+
+private ValgAttributType getAttributTypeValg(Element empEl){
+
+	//for hver <attributTypeValg> element hent text og int værdier
+	
+	String navn = getTextValue(empEl, "type");
+	String beregner = getTextValue(empEl, "beregner");
+	String placering = getTextValue(empEl, "placering");
+	String feltnavn = getTextValue(empEl, "feltnavn");
+	String feltpris = getTextValue(empEl, "feltpris");
+	
+	feltNavne.add("navn");
+	feltNavne.add("pris");
+	attributter.add(feltnavn);
+	attributter.add(feltpris);
+	
+	//ValgAttributType va = new ValgAttributType(navn, feltNavne, beregner);
+	
+}
+
+private String getTextValue(Element ele, String tagNavn) {
+	String textVal = null;
+	NodeList nl = ((org.w3c.dom.Element) ele).getElementsByTagName(tagNavn);
+	if(nl !=null && nl.getLength()> 0){
+		Element el = (Element)nl.item(0);
+		textVal = ((Node) el).getFirstChild().getNodeValue();
+	}
+	return textVal;
+}
+
 
 }
 
